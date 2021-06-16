a2528632fb40eb22d1197c97345914cd63d4376d
Dragon Flip <62544653+dragonflip@users.noreply.github.com>
2021-06-16 09:34:13 +0300

    Update King.h

--- King.h
+++ King.h
@@ -113,11 +113,11 @@ public:
 			cout << "CHECKMATE: " << checkMate << "\n\n";
 		}
 		else {
-			int piecesCanMove = 0;
+			/*int piecesCanMove = 0;
 			for (int i = 0; i < 8; i++) {
 				for (int j = 0; j < 8; j++) {
 					if (board[i][j]->type != '0' && board[i][j]->white == this->white) {
-						if (board[i][j]->moves.size() > 0) {
+						if (board[i][j]->moves.size() > 0 && this->moves.size() > 0) {
 							piecesCanMove++;
 						}
 					}
@@ -127,7 +127,7 @@ public:
 				pat = true;
 			}
 
-			cout << "PAT: " << pat << "\n\n";
+			cout << "PAT: " << pat << "\n\n";*/
 		}
 
 		//this->ifCheck();
